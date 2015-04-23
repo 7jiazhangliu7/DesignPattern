2832869a42241a85a7f65f990442bcac1eb61c48
7jiazhangliu7 <77jiazhangliu77@gmail.com>
2015-04-23 16:10:24 -0500

    Create Singleton.java

---
+++ Singleton-Pattern/Singleton.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Jiazhang_Liu on 4/23/2015.
+ */
+public class Singleton {
+    private static Singleton instance;
+    private String str = "";
+
+    private Singleton(String str){
+        this.str = str;
+    }
+
+    public static Singleton getInstance(String str) {
+        if(instance == null) {
+            instance = new Singleton(str);
+        }
+        return instance;
+    }
+
+    public void print() {
+        System.err.println(str + "!!!");
+    }
+
+}
